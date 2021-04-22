94705311c17f162a8ded54600ce7b4190c3e7fab
dev-and-web <contact@devandweb.fr>
2021-04-22 12:45:58 +0300

    first commitù

---
+++ README.md
@@ -0,0 +1,21 @@
+# Lang FR pour : Laravel 5.8 / Laravel 6 / Laravel 7 - Traduction française / French translation
+
+
+
+
+### Auteur
+
+Package mis en ligne par :
+[Développeur web Freelance](https://www.devandweb.fr)
+[![Developpeur PHP Freelance](https://www.devandweb.fr/medias/app/website/developpeur-web.png)](https://www.devandweb.fr/freelance/developpeur-php)
+
+
+
+
+
+
+### Détails
+
+Dans ce package, vous avez toutes les traductions françaises pour les versions 5.8 / 6 / 7 du Framework PHP Laravel.
+
+Le dossier **fr** doit être rangé dans le path **resources/lang** de votre projet Laravel.
---
+++ laravel-7/src/fr/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ces certificats ne correspondent pas à nos enregistrements',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez essayer de nouveau dans :seconds secondes.',
+
+    "login_success" => "Vous êtes connecté.",
+    "logout_success" => "Vous êtes déconnecté.",
+
+];
